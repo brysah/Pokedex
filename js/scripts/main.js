780be73e24a1b188294ae9a4bdfc587cca19da42
Brysah <brysahvidal@gmail.com>
2023-04-19 14:08:27 -0300

    FEAT: Adiciona script js para abrir e fechar o modal

---
+++ js/scripts/main.js
@@ -0,0 +1,17 @@
+const cardPokemon = document.querySelectorAll('.js-open-details-pokemon');
+const btnCloseModal = document.querySelector('.js-close-modal');
+
+function openDetailsPokemon(){
+   document.documentElement.classList.add('open-modal');
+}
+
+function closeDetailsPokemon(){
+    document.documentElement.classList.remove('open-modal');
+}
+
+cardPokemon.forEach(card => {
+    card.addEventListener('click',openDetailsPokemon);
+    
+})
+
+btnCloseModal.addEventListener('click',closeDetailsPokemon);
